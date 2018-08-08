c4a847c02df9a8ecc40847533d35f7a62cbf9446
Zvezda <hqzvezda@gmail.com>
2018-08-08 14:18:44 +0800

    Initial commit

---
+++ lib/AutoScrollPageViewDemo.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'AutoScrollPageView.dart';
+
+class AutoScrollPageViewDemo extends StatelessWidget
+{
+    @override
+    Widget build(BuildContext context)
+    {
+        // TODO: implement build
+        List<Widget> widgetList = [];
+        widgetList.add(new Center(child: new Container(color: Colors.orange, width: 300.0, height: 300.0, child: new Text("A"),),));
+        widgetList.add(new Center(child: new Container(color: Colors.orange, width: 300.0, height: 300.0, child: new Text("B"),),));
+        widgetList.add(new Center(child: new Container(color: Colors.orange, width: 300.0, height: 300.0, child: new Text("C"),),));
+        //widgetList.add(new Center(child: new Container(color: Colors.orange, width: 300.0, height: 300.0, child: new Text("D"),),));
+
+        PageChanged pageChanged = (int index)
+        {
+            print("当前下标----------------------->$index");
+        };
+
+        AutoScrollPageView autoScrollPageView = AutoScrollPageView(widgetList, pageChanged: pageChanged,);
+        return new Center(child: autoScrollPageView);
+    }
+}
+
